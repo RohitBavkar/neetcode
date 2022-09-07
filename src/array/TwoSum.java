43add9ba55cc05ed8a371e89bab57aeead4d87dc
Rohit Bavkar <rohitbavkar95@gmail.com>
2022-09-07 17:03:34 +0530

    Valid Anagram

--- src/array/TwoSum.java
+++ src/array/TwoSum.java
@@ -4,10 +4,8 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
-/*
-  Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
-  https://leetcode.com/problems/two-sum/
-*/
+//https://leetcode.com/problems/two-sum/
+
 public class TwoSum {
 
     public int[] twoSum(int[] nums, int target) {
---
+++ src/array/ValidAnagram.java
@@ -0,0 +1,27 @@
+package array;
+
+import java.util.Arrays;
+
+//https://leetcode.com/problems/valid-anagram/
+public class ValidAnagram {
+    public boolean isAnagram(String s, String t) {
+
+        if((s.length() == 1 || t.length() == 1) && s.equalsIgnoreCase(t))
+            return true;
+
+        char []sArray = s.toCharArray();
+        char []tArray = t.toCharArray();
+        Arrays.sort(sArray);
+        Arrays.sort(tArray);
+        if(String.valueOf(sArray).equalsIgnoreCase(String.valueOf(tArray)))
+            return true;
+        else
+            return false;
+
+    }
+
+    public static void main(String[] args) {
+        ValidAnagram va = new ValidAnagram();
+        System.out.println(va.isAnagram("anagram", "nagaram"));
+    }
+}
